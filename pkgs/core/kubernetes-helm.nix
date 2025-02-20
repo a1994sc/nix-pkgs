a9434df293dd6fe5aae6b92932c888cc9c7040c4
Allen Conlon <software@conlon.dev>
2025-02-20 17:45:40 -0500

    feat(add): helm

---
+++ pkgs/core/kubernetes-helm.nix
@@ -0,0 +1,76 @@
+{
+  # keep-sorted start
+  buildGoModule,
+  fetchFromGitHub,
+  installShellFiles,
+  lib,
+  # keep-sorted end
+  ...
+}:
+let
+  # keep-sorted start prefix_order=pname,version,
+  pname = "helm";
+  version = "3.17.1";
+  sha256 = "sha256-j30ODWM75x2LV8vihLGeOMj9mnOUp7XaFOsp+/csEb4=";
+  vendorHash = "sha256-dZN1rOcGDIvhqXi35904kTBQ8RPeS1PFgspzn/j1ZQU=";
+  # keep-sorted end
+  rev = "v" + version;
+in
+buildGoModule rec {
+  inherit version pname vendorHash;
+
+  src = fetchFromGitHub {
+    inherit rev sha256;
+    owner = "helm";
+    repo = pname;
+    leaveDotGit = true;
+    postFetch = ''
+      cd "$out"
+      git rev-parse HEAD > $out/COMMIT
+      find $out -name .git -print0 | xargs -0 rm -rf
+    '';
+  };
+
+  ldflags = [
+    "-w"
+    "-s"
+    "-X helm.sh/helm/v3/internal/version.version=v${version}"
+    "-X helm.sh/helm/v3/internal/version.gitTreeState=clean"
+  ];
+
+  preBuild = ''
+    # set k8s version to client-go version, to match upstream
+    K8S_MODULES_VER="$(go list -f '{{.Version}}' -m k8s.io/client-go)"
+    K8S_MODULES_MAJOR_VER="$(($(cut -d. -f1 <<<"$K8S_MODULES_VER") + 1))"
+    K8S_MODULES_MINOR_VER="$(cut -d. -f2 <<<"$K8S_MODULES_VER")"
+
+    ldflags+=" -X helm.sh/helm/v3/internal/version.gitCommit=$(cat COMMIT)"
+    ldflags+=" -X helm.sh/helm/v3/pkg/lint/rules.k8sVersionMajor=''${K8S_MODULES_MAJOR_VER}"
+    ldflags+=" -X helm.sh/helm/v3/pkg/lint/rules.k8sVersionMinor=''${K8S_MODULES_MINOR_VER}"
+    ldflags+=" -X helm.sh/helm/v3/pkg/chartutil.k8sVersionMajor=''${K8S_MODULES_MAJOR_VER}"
+    ldflags+=" -X helm.sh/helm/v3/pkg/chartutil.k8sVersionMinor=''${K8S_MODULES_MINOR_VER}"
+  '';
+
+  doCheck = false;
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  env.CGO_ENABLED = 0;
+
+  postInstall = ''
+    for shell in bash fish zsh; do
+      $out/bin/helm completion $shell > helm.$shell
+      installShellCompletion helm.$shell
+    done
+  '';
+
+  meta = with lib; {
+    # keep-sorted start
+    description = "A package manager for kubernetes";
+    homepage = "https://github.com/helm/helm";
+    license = [ licenses.asl20 ];
+    mainProgram = "helm";
+    platforms = platforms.linux;
+    # keep-sorted end
+  };
+}
