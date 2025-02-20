c08373e31eaf220b5fe95d1f075347825a5c2864
Allen Conlon <software@conlon.dev>
2025-02-20 17:50:13 -0500

    feat(add): lazysql

---
+++ pkgs/core/lazysql.nix
@@ -0,0 +1,54 @@
+{
+  # keep-sorted start
+  buildGoModule,
+  darwin ? null,
+  fetchFromGitHub,
+  lazysql,
+  lib,
+  stdenv,
+  testers,
+  xorg ? null,
+# keep-sorted end
+}:
+let
+  # keep-sorted start prefix_order=pname,version,
+  pname = "lazysql";
+  version = "0.3.5";
+  sha256 = "sha256-jFQvtC6D7B0p+zh3Mv3QjMTHIolc1VbKqEpXe0WxQAM=";
+  vendorHash = "sha256-LLOUTml/mz7edCUy82k+S5PfpFovgUTQr0BoQQXiVGs=";
+  # keep-sorted end
+  rev = "v" + version;
+in
+buildGoModule rec {
+  inherit version pname vendorHash;
+
+  src = fetchFromGitHub {
+    inherit rev sha256;
+    owner = "jorgerojas26";
+    repo = "lazysql";
+  };
+
+  ldflags = [
+    "-X main.version=${version}"
+  ];
+
+  env.CGO_ENABLED = 0;
+
+  buildInputs =
+    lib.optionals stdenv.hostPlatform.isLinux [ xorg.libX11 ]
+    ++ lib.optionals stdenv.hostPlatform.isDarwin [ darwin.apple_sdk.frameworks.Cocoa ];
+
+  passthru.tests.version = testers.testVersion {
+    package = lazysql;
+    command = "lazysql version";
+  };
+
+  meta = with lib; {
+    # keep-sorted start
+    description = "A cross-platform TUI database management tool written in Go";
+    homepage = "https://github.com/jorgerojas26/lazysql";
+    license = [ licenses.mit ];
+    mainProgram = "lazysql";
+    # keep-sorted end
+  };
+}
