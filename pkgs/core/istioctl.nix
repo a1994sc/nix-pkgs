29b00a39dedc66419a39f6d53306b59a56a8defc
Allen Conlon <software@conlon.dev>
2025-02-20 17:49:03 -0500

    feat(add): istioctl

---
+++ pkgs/core/istioctl.nix
@@ -0,0 +1,73 @@
+{
+  # keep-sorted start
+  buildGoModule,
+  fetchFromGitHub,
+  installShellFiles,
+  lib,
+  # keep-sorted end
+  ...
+}:
+let
+  # keep-sorted start prefix_order=pname,version,
+  pname = "istioctl";
+  version = "1.24.3";
+  sha256 = "sha256-/ABy0zZF/+rxYkpS5H6EAXZVYZxt7I4SCjLtrtkacGw=";
+  vendorHash = "sha256-B4MxvOzKxsQJmc9dKQFt1CGi1PnElOt8H72H2JVaZUU=";
+  # keep-sorted end
+  rev = "v" + version;
+in
+buildGoModule rec {
+  inherit version pname vendorHash;
+
+  src = fetchFromGitHub {
+    inherit sha256;
+    owner = "istio";
+    repo = "istio";
+    rev = version;
+  };
+
+  nativeBuildInputs = [
+    installShellFiles
+  ];
+
+  ldflags =
+    let
+      attrs = [
+        "istio.io/istio/pkg/version.buildVersion=${version}"
+        "istio.io/istio/pkg/version.buildStatus=Nix"
+        "istio.io/istio/pkg/version.buildTag=${version}"
+        "istio.io/istio/pkg/version.buildHub=docker.io/istio"
+      ];
+    in
+    [
+      "-s"
+      "-w"
+      "${lib.concatMapStringsSep " " (attr: "-X ${attr}") attrs}"
+    ];
+
+  subPackages = [ "istioctl/cmd/istioctl" ];
+
+  doInstallCheck = true;
+  installCheckPhase = ''
+    $out/bin/istioctl version --remote=false | grep ${version} > /dev/null
+  '';
+
+  postInstall = ''
+    $out/bin/istioctl collateral --man --bash --zsh
+    installManPage *.1
+    installShellCompletion istioctl.bash
+    installShellCompletion --zsh _istioctl
+  '';
+
+  env.CGO_ENABLED = 0;
+
+  meta = with lib; {
+    # keep-sorted start
+    description = "Istio configuration command line utility for service operators to debug and diagnose their Istio mesh";
+    homepage = "https://istio.io/latest/docs/reference/commands/istioctl";
+    license = [ licenses.asl20 ];
+    mainProgram = "istioctl";
+    platforms = platforms.linux;
+    # keep-sorted end
+  };
+}
