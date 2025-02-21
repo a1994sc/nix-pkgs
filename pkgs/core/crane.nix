4a37616ea14f6a57e2177ac726964e0bbe2948d9
Allen Conlon <software@conlon.dev>
2025-02-21 18:46:50 -0500

    feat(add): crane

---
+++ pkgs/core/crane.nix
@@ -0,0 +1,78 @@
+{
+  # keep-sorted start
+  buildGoModule,
+  fetchFromGitHub,
+  installShellFiles,
+  lib,
+  # keep-sorted end
+  ...
+}:
+let
+  # keep-sorted start prefix_order=pname,version,
+  pname = "go-containerregistry";
+  version = "0.20.3";
+  sha256 = "sha256-HiksVzVuY4uub7Lwfyh3GN8wpH2MgIjKSO4mQJZeNvs=";
+  vendorHash = null;
+  # keep-sorted end
+  rev = "v" + version;
+  bins = [
+    "crane"
+    "gcrane"
+  ];
+in
+buildGoModule rec {
+  inherit version pname vendorHash;
+
+  src = fetchFromGitHub {
+    inherit rev sha256;
+    owner = "google";
+    repo = pname;
+  };
+
+  subPackages = [
+    "cmd/crane"
+    "cmd/gcrane"
+  ];
+
+  outputs = [ "out" ] ++ bins;
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  env.CGO_ENABLED = 0;
+
+  ldflags =
+    let
+      t = "github.com/google/go-containerregistry";
+    in
+    [
+      "-s"
+      "-w"
+      "-X ${t}/cmd/crane/cmd.Version=v${version}"
+      "-X ${t}/pkg/v1/remote/transport.Version=${version}"
+    ];
+
+  postInstall = lib.concatStringsSep "\n" (
+    map (bin: ''
+      mkdir -p ''$${bin}/bin &&
+      mv $out/bin/${bin} ''$${bin}/bin/ &&
+      ln -s ''$${bin}/bin/${bin} $out/bin/
+
+      for shell in bash fish zsh; do
+        $out/bin/${bin} completion $shell > ${bin}.$shell
+        installShellCompletion ${bin}.$shell
+      done
+    '') bins
+  );
+
+  doCheck = false;
+
+  meta = with lib; {
+    # keep-sorted start
+    description = "Tools for interacting with remote images and registries including crane and gcrane";
+    homepage = "https://github.com/google/go-containerregistry";
+    license = [ licenses.asl20 ];
+    mainProgram = "crane";
+    platforms = platforms.linux;
+    # keep-sorted end
+  };
+}
