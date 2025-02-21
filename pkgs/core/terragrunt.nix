f21300468ea451ced7c295fc1b81f019bd1576dd
Allen Conlon <software@conlon.dev>
2025-02-21 18:51:50 -0500

    feat(re-add): terragrunt

---
+++ pkgs/core/terragrunt.nix
@@ -0,0 +1,71 @@
+{
+  # keep-sorted start
+  buildGo123Module,
+  fetchFromGitHub,
+  go-mockery,
+  installShellFiles,
+  lib,
+  # keep-sorted end
+  ...
+}:
+let
+  # keep-sorted start prefix_order=pname,version,
+  pname = "terragrunt";
+  version = "0.72.6";
+  sha256 = "sha256-dlEyeDh5OkERboEUPhe/jYYEc7jnGZL6PhW9Tu6vxcs=";
+  vendorHash = "sha256-hgjVmbM0qh8o1e2UaU9Q0GURzC8XPthLAuXZ+eCuflU=";
+  # keep-sorted end
+  rev = "v" + version;
+in
+buildGo123Module rec {
+  inherit version pname vendorHash;
+
+  src = fetchFromGitHub {
+    inherit rev sha256;
+    owner = "gruntwork-io";
+    repo = pname;
+  };
+
+  preBuild = ''
+    make generate-mocks
+  '';
+
+  doCheck = false;
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/gruntwork-io/go-commons/version.Version=v${version}"
+  ];
+
+  doInstallCheck = true;
+
+  installCheckPhase = ''
+    runHook preInstallCheck
+    $out/bin/terragrunt --help
+    $out/bin/terragrunt --version | grep "v${version}"
+    runHook postInstallCheck
+  '';
+
+  nativeBuildInputs = [
+    installShellFiles
+    go-mockery
+  ];
+
+  env.CGO_ENABLED = 0;
+
+  postInstall = ''
+    installShellCompletion --bash --name terragrunt <(echo complete -C $out/bin/terragrunt terragrunt)
+  '';
+
+  meta = with lib; {
+    # keep-sorted start
+    changelog = "https://github.com/gruntwork-io/terragrunt/releases/tag/v${version}";
+    description = "A thin wrapper for Terraform that supports locking for Terraform state and enforces best practices";
+    homepage = "https://github.com/gruntwork-io/terragrunt";
+    license = [ licenses.mit ];
+    mainProgram = "terragrunt";
+    platforms = platforms.linux;
+    # keep-sorted end
+  };
+}
