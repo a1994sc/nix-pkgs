51a7f6b1d0c585a49e8e83b8abbfd0c64befd231
Allen Conlon <software@conlon.dev>
2025-02-21 18:59:38 -0500

    feat(add): opentofu

---
+++ pkgs/core/uds.nix
@@ -0,0 +1,84 @@
+{
+  # keep-sorted start
+  buildGoModule,
+  fetchFromGitHub,
+  installShellFiles,
+  lib,
+  # keep-sorted end
+  ...
+}:
+let
+  # keep-sorted start prefix_order=pname,version,
+  pname = "uds";
+  version = "0.21.0";
+  sha256 = "";
+  vendorHash = "";
+  # keep-sorted end
+  rev = "v" + version;
+  flag = {
+    zarf = "github.com/defenseunicorns/zarf/src";
+    uds = "github.com/defenseunicorns/uds-cli/src";
+    k9s = "github.com/derailed/k9s";
+    google = "github.com/google/go-containerregistry/cmd/crane";
+  };
+in
+buildGoModule rec {
+  inherit version pname vendorHash;
+
+  src = fetchFromGitHub {
+    inherit rev sha256;
+    owner = "defenseunicorns";
+    repo = "uds-cli";
+  };
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  env.CGO_ENABLED = 0;
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X ${flag.uds}/config.CLIVersion=v${version}"
+    "-X ${flag.zarf}/config.ActionsCommandZarfPrefix=zarf"
+    "-X ${flag.k9s}/cmd.version=$(go list -f '{{.Version}}' -m github.com/derailed/k9s)"
+    "-X ${flag.google}/cmd.Version=$(go list -f '{{.Version}}' -m github.com/google/go-containerregistry)"
+    "-X ${flag.zarf}/cmd/tools.syftVersion=$(go list -f '{{.Version}}' -m github.com/anchore/syft)"
+    "-X ${flag.zarf}/cmd/tools.archiverVersion=$(go list -f '{{.Version}}' -m github.com/mholt/archiver/v3)"
+    "-X ${flag.zarf}/cmd/tools.helmVersion=$(go list -f '{{.Version}}' -m helm.sh/helm/v3)"
+  ];
+
+  subPackages = [ "." ];
+
+  doCheck = false;
+
+  buildPhase =
+    let
+      args = builtins.concatStringsSep " " ldflags;
+    in
+    ''
+      go build -o uds -ldflags="${args}" main.go
+    '';
+
+  installPhase = ''
+    install -Dm755 uds -t $out/bin
+    runHook postInstall
+  '';
+
+  postInstall = ''
+    export K9S_LOGS_DIR=$(mktemp -d)
+    for shell in bash fish zsh; do
+      $out/bin/uds completion --no-log-file $shell > uds.$shell
+      installShellCompletion uds.$shell
+    done
+  '';
+
+  meta = with lib; {
+    # keep-sorted start
+    description = "DevSecOps for Air Gap & Limited-Connection Systems";
+    homepage = "https://github.com/defenseunicorns/uds-cli";
+    license = [ licenses.asl20 ];
+    mainProgram = "uds";
+    platforms = platforms.linux;
+    # keep-sorted end
+  };
+}
