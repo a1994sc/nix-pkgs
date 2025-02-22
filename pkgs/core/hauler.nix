03911413a35600fa444153ca40a9970f4bc1a5bb
Allen Conlon <software@conlon.dev>
2025-02-22 18:04:57 -0500

    feat(add): hauler

---
+++ pkgs/core/hauler.nix
@@ -0,0 +1,95 @@
+{
+  # keep-sorted start
+  buildGoModule,
+  fetchFromGitHub,
+  fetchurl,
+  installShellFiles,
+  lib,
+  stdenv,
+  system,
+  cosign-hauler,
+  # keep-sorted end
+  ...
+}:
+let
+  # keep-sorted start prefix_order=pname,version,
+  pname = "hauler";
+  version = "1.2.1";
+  sha256 = "sha256-xC94yAkgsnWC3/9SjHtZAGiGEgvVF8o6ag8Q1l/TWe0=";
+  vendorHash = "sha256-sZGLxyBpxl166p0kNXD0gheMpyfP6TZmWwMgUwJ194Q=";
+  # keep-sorted end
+  rev = "v" + version;
+  flag = {
+    hauler = "github.com/rancherfederal/hauler/internal/version";
+  };
+
+  arch = if system == "x86_64-linux" then "amd64" else "arm64";
+  os = if stdenv.isLinux then "linux" else "darwin";
+in
+buildGoModule rec {
+  inherit version pname vendorHash;
+
+  src = fetchFromGitHub {
+    inherit rev sha256;
+    owner = "hauler-dev";
+    repo = pname;
+    leaveDotGit = true;
+    postFetch = ''
+      cd "$out"
+      git rev-parse HEAD > $out/COMMIT
+      date -u -d "@$(git log -1 --pretty=%ct)" +'%Y-%m-%dT%H:%M:%SZ' > $out/SOURCE_DATE_EPOCH
+      find $out -name .git -print0 | xargs -0 rm -rf
+    '';
+  };
+
+  preBuild = ''
+    mkdir -p cmd/hauler/binaries
+
+    cp ${cosign-hauler}/bin/cosign cmd/hauler/binaries/cosign-${os}-${arch}
+  '';
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X '${flag.hauler}.gitTreeState=clean'"
+    "-X '${flag.hauler}.gitVersion=${version}'"
+    "-X ${flag.hauler}.gitCommit=$(cat COMMIT)"
+    "-X ${flag.hauler}.buildDate=$(cat SOURCE_DATE_EPOCH)"
+  ];
+
+  buildPhase =
+    let
+      args = builtins.concatStringsSep " " ldflags;
+    in
+    ''
+      runHook preBuild
+
+      go build -o hauler -ldflags="${args}" cmd/hauler/main.go
+    '';
+
+  installPhase = ''
+    install -Dm755 hauler -t $out/bin
+
+    runHook postInstall
+  '';
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  env.CGO_ENABLED = 0;
+
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    installShellCompletion --cmd hauler \
+      --bash <($out/bin/hauler completion bash) \
+      --fish <($out/bin/hauler completion fish) \
+      --zsh  <($out/bin/hauler completion zsh)
+  '';
+
+  meta = with lib; {
+    # keep-sorted start
+    description = "Airgap Container Swiss Army Knife ";
+    homepage = "https://github.com/hauler-dev/hauler";
+    license = [ licenses.asl20 ];
+    mainProgram = "hauler";
+    # keep-sorted end
+  };
+}
