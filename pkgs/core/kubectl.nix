d492c84860621752b7282881925a954e34a12f67
Allen Conlon <software@conlon.dev>
2025-02-21 19:11:02 -0500

    feat(add): kubectl

---
+++ pkgs/core/kubectl.nix
@@ -0,0 +1,89 @@
+{
+  # keep-sorted start
+  buildGoModule,
+  fetchFromGitHub,
+  installShellFiles,
+  lib,
+  makeWrapper,
+  rsync,
+  runtimeShell,
+  which,
+  # keep-sorted end
+  ...
+}:
+let
+  # keep-sorted start prefix_order=pname,version,
+  pname = "kubectl";
+  version = "1.32.2";
+  sha256 = "sha256-pie36Y3zKGKvnCDHtjNHYox1b2xhy6w7MShkAfkDVrs=";
+  vendorHash = null;
+  # keep-sorted end
+  rev = "v" + version;
+in
+buildGoModule rec {
+  inherit version pname vendorHash;
+
+  src = fetchFromGitHub {
+    inherit rev sha256;
+    owner = "kubernetes";
+    repo = "kubernetes";
+  };
+
+  outputs = [
+    "out"
+    "man"
+    "convert"
+  ];
+
+  WHAT = lib.concatStringsSep " " [
+    "cmd/${pname}"
+    "cmd/${pname}-convert"
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+    substituteInPlace "hack/update-generated-docs.sh" --replace "make" "make SHELL=${runtimeShell}"
+    patchShebangs ./hack ./cluster/addons/addon-manager
+    make "SHELL=${runtimeShell}" "WHAT=$WHAT"
+    ./hack/update-generated-docs.sh
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    install -D _output/local/go/bin/kubectl -t $out/bin
+    install -D _output/local/go/bin/kubectl-convert -t $convert/bin
+
+    installManPage docs/man/man1/kubectl*
+
+    for shell in bash fish zsh; do
+      $out/bin/kubectl completion $shell > kubectl.$shell
+      installShellCompletion kubectl.$shell
+    done
+
+    runHook postInstall
+  '';
+
+  GOWORK = "off";
+
+  env.CGO_ENABLED = 0;
+
+  nativeBuildInputs = [
+    makeWrapper
+    which
+    rsync
+    installShellFiles
+  ];
+
+  doCheck = false;
+
+  meta = with lib; {
+    # keep-sorted start
+    description = "Kubernetes CLI";
+    homepage = "https://github.com/kubernetes/kubernetes";
+    license = [ licenses.asl20 ];
+    mainProgram = "kubectl";
+    platforms = lib.platforms.unix;
+    # keep-sorted end
+  };
+}
