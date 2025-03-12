054afb89f7a079325d22e79d1ca010fda794c0ec
Allen Conlon <software@conlon.dev>
2025-03-12 11:04:40 -0400

    pkg: update helm values schema

--- pkgs/core/kubernetes-helmPlugins-values-schema-json.nix
+++ pkgs/core/kubernetes-helmPlugins-values-schema-json.nix
@@ -9,8 +9,8 @@
 let
   # keep-sorted start prefix_order=pname,version,
   pname = "helm-values-schema-json";
-  version = "1.7.0";
-  sha256 = "sha256-P/3EcVBo11XxY+S8FyDiSUPQNfgTTqLDmbbc7Up5LNc=";
+  version = "1.7.2";
+  sha256 = "sha256-I8iab/dqfB8JGvOvrH2Vxe8eBRklwXWbWAxOOCAsIQI=";
   vendorHash = "sha256-mT2A6xXlTFYrA6yNpz9jaa69vdetY/OgjNtTvG4jAYs=";
   # keep-sorted end
   rev = "v" + version;
