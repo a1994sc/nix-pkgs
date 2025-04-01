caf86b7ca2cfece9377f6d8ca11ad660922c542c
Allen Conlon <software@conlon.dev>
2025-04-01 18:21:18 -0400

    pkg: updated sops

--- pkgs/core/sops/package.nix
+++ pkgs/core/sops/package.nix
@@ -14,9 +14,9 @@
 let
   # keep-sorted start prefix_order=pname,version,
   pname = "sops";
-  version = "3.9.4";
-  sha256 = "sha256-w2RMK1Fl/k8QXV68j0Kc6shtx4vQa07RCnpgHLM8c8Q=";
-  vendorHash = "sha256-wxmSj3QaFChGE+/2my7Oe2mhprwi404izUxteecyggY=";
+  version = "3.10.1";
+  sha256 = "sha256-LdsuN243oQ/L6LYgynb7Kw60alXn5IfUfhY0WaZFVCU=";
+  vendorHash = "sha256-I+iwimrNdKABZFP2etZTQJAXKigh+0g/Jhip86Cl5Rg=";
   # keep-sorted end
   rev = "v" + version;
 in
