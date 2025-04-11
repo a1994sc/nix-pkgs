34dc7b1a238a4bb2651ba276ac03a56cb958446e
Allen Conlon <software@conlon.dev>
2025-04-11 18:25:55 -0400

    chore: break out fluxcd logic

--- pkgs/multi/fluxcd/2-3.nix
+++ pkgs/multi/fluxcd/2-3.nix
@@ -1,6 +1,7 @@
 {
   # keep-sorted start
   buildGoModule,
+  callPackage,
   fetchFromGitHub,
   fetchzip,
   installShellFiles,
@@ -10,77 +11,26 @@
   ...
 }:
 let
-  # keep-sorted start prefix_order=pname,version,
-  pname = "fluxcd";
+  # keep-sorted start prefix_order=version,
   version = "2.3.0";
   manifestsSha256 = "sha256-PdhR+UDquIJWtpSymtT6V7qO5fVJOkFz6RGzAx7xeb4=";
   sha256 = "sha256-ZQs1rWI31qDo/BgjrmiNnEdR2OL8bUHVz+j5VceEp2k=";
   vendorHash = "sha256-0YH3pgFrsnh5jIsZpj/sIgfiOCTtIlPltMS5mdGz1eM=";
   # keep-sorted end
-  rev = "v" + version;
-  manifests = fetchzip {
-    url = "https://github.com/fluxcd/flux2/releases/download/v${version}/manifests.tar.gz";
-    sha256 = manifestsSha256;
-    stripRoot = false;
-  };
 in
-buildGoModule rec {
-  inherit version pname vendorHash;
-
-  src = fetchFromGitHub {
-    inherit rev sha256;
-    owner = "fluxcd";
-    repo = "flux2";
-  };
-
-  postUnpack = ''
-    cp -r ${manifests} source/cmd/flux/manifests
-
-    # disable tests that require network access
-    rm source/cmd/flux/create_secret_git_test.go
-  '';
-
-  ldflags = [
-    "-s"
-    "-w"
-    "-X main.VERSION=${version}"
-  ];
-
-  subPackages = [ "cmd/flux" ];
-
-  # Required to workaround test error:
-  #   panic: mkdir /homeless-shelter: permission denied
-  HOME = "$TMPDIR";
-  env.CGO_ENABLED = 0;
-
-  nativeBuildInputs = [ installShellFiles ];
-
-  doInstallCheck = true;
-  installCheckPhase = ''
-    $out/bin/flux --version | grep ${version} > /dev/null
-  '';
-
-  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-    installShellCompletion --cmd flux \
-      --bash <($out/bin/flux completion bash) \
-      --fish <($out/bin/flux completion fish) \
-      --zsh  <($out/bin/flux completion zsh)
-  '';
-
-  passthru.updateScript = ./update.sh;
-
-  meta = with lib; {
+callPackage ./. {
+  inherit
     # keep-sorted start
-    description = "Open and extensible continuous delivery solution for Kubernetes";
-    downloadPage = "https://github.com/fluxcd/flux2/releases/tag/v${version}";
-    homepage = "https://fluxcd.io";
-    license = [ licenses.asl20 ];
-    mainProgram = "flux";
+    buildGoModule
+    fetchFromGitHub
+    fetchzip
+    installShellFiles
+    lib
+    manifestsSha256
+    sha256
+    stdenv
+    vendorHash
+    version
     # keep-sorted end
-    longDescription = ''
-      Flux is a tool for keeping Kubernetes clusters in sync
-      with sources of configuration (like Git repositories), and automating
-      updates to configuration when there is new code to deploy.
-    '';
-  };
+    ;
 }
