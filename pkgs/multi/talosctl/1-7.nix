75fc20bbdcaf1be392aaaf2c583d2eb45749157a
Allen Conlon <software@conlon.dev>
2025-04-11 18:26:10 -0400

    chore: break out talos logic

--- pkgs/multi/talosctl/1-7.nix
+++ pkgs/multi/talosctl/1-7.nix
@@ -5,67 +5,28 @@
   installShellFiles,
   lib,
   stdenv,
+  callPackage,
   # keep-sorted end
   ...
 }:
 let
-  # keep-sorted start prefix_order=pname,version,
-  pname = "talosctl";
+  # keep-sorted start prefix_order=version,
   version = "1.7.7";
   sha256 = "sha256-gihVZX1wwlpV9w/pydV9wQLQVbBCeIwOVMwd2PdI9Nw=";
   vendorHash = "sha256-6KSa1XEAVPI1Pyhatsv8vhzY+1G/Gt426iS3yqNhjCg=";
   # keep-sorted end
-  rev = "v" + version;
 in
-buildGoModule rec {
-  inherit version pname vendorHash;
-
-  src = fetchFromGitHub {
-    inherit rev sha256;
-    owner = "siderolabs";
-    repo = "talos";
-
-    leaveDotGit = true;
-    postFetch = ''
-      cd "$out"
-      git describe --match=none --always --abbrev=8 > $out/COMMIT
-      find $out -name .git -print0 | xargs -0 rm -rf
-    '';
-  };
-
-  ldflags = [
-    "-s"
-    "-w"
-  ];
-
-  GOEXPERIMENT = "loopvar";
-  env.CGO_ENABLED = 0;
-
-  subPackages = [ "cmd/${pname}" ];
-
-  preBuild = ''
-    cat COMMIT | tr -d "\n" > pkg/machinery/gendata/data/sha
-    export GOWORK="off"
-  '';
-
-  nativeBuildInputs = [ installShellFiles ];
-
-  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-    installShellCompletion --cmd ${pname} \
-      --bash <($out/bin/${pname} completion bash) \
-      --fish <($out/bin/${pname} completion fish) \
-      --zsh  <($out/bin/${pname} completion zsh)
-  '';
-
-  doCheck = false;
-
-  meta = with lib; {
+callPackage ./. {
+  inherit
     # keep-sorted start
-    description = "A CLI for out-of-band management of Kubernetes nodes created by Talos";
-    homepage = "https://www.talos.dev/";
-    license = [ licenses.mpl20 ];
-    mainProgram = "talosctl";
-    platforms = platforms.linux;
+    buildGoModule
+    fetchFromGitHub
+    installShellFiles
+    lib
+    sha256
+    stdenv
+    vendorHash
+    version
     # keep-sorted end
-  };
+    ;
 }
